6aa4b4a4a14cc00442b9141bf20420a82bbb3c2a
RoudroMehedi <roudro.m@gmail.com>
2023-08-03 23:37:12 +0200

    Api link to Cyclic

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,2 @@
 // config.js
-export const BASE_API_URL = "https://live-cinema.onrender.com"; // Replace this with your actual API URL
+export const BASE_API_URL = "https://tricky-turtleneck-shirt-toad.cyclic.app"; // Replace this with your actual API URL
